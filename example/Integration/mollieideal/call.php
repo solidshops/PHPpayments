f9ad30d1451c0c6510c8a9bcb9079439c21ec466
driesdroesbeke <dries@solidshops.com>
2013-01-05 12:27:21 +0100

    Bugfixes ideal

--- example/Integration/mollieideal/call.php
+++ example/Integration/mollieideal/call.php
@@ -3,14 +3,13 @@
 include_once '../../../lib/PHPpayments/Loader.php';
 
 $domain = "http://www.domain.com";
-$paymentmethod = "Payment_Integration_Ogone";
+$paymentmethod = "Payment_Integration_Mollieideal";
 
 $obj_payment = PHPpayments_Loader::load ( $paymentmethod );
 
 //set credentials
-$obj_payment->addFieldSetting ( "account", "???PSPID???" );
-$obj_payment->addFieldSetting ( "secret", "???SHA-IN Pass phrase???");
-$obj_payment->addFieldSetting ( "secret2", "???SHA-OUT Pass phrase???");
+$obj_payment->addFieldSetting ( "account", "559477" );
+$obj_payment->addFieldSetting ( "secret", "9815bd2f");
 
 //include dummy order data
 include_once "../../orderdata.php";
@@ -21,8 +20,12 @@ $obj_payment->setUrlSuccess ( $domain . "/confirmorder/guid/" . $obj_payment->ar
 $obj_payment->setUrlCancel ( $domain . "/cancelorder/guid/" . $obj_payment->arr_order['guid'] );
 $obj_payment->setUrlCallback ( $domain . "/callback" );
 
+
 //enable sandbox environment
 $obj_payment->enableTestMode ();
+
+//set extra payment options specific bank in this case
+$obj_payment->setPaymentOption ( "9999");
 
 //map our data structure with payment providers structure
 $obj_payment->preparePayment ();
