18c66158aa7444100f7e0083676892536171187e
Dries Droesbeke <dries@solidshops.com>
2013-12-04 00:16:52 +0100

    Offline methods namespace

--- src/PHPpayments/Payment/Offline/Banktransfer.php
+++ src/PHPpayments/Payment/Offline/Banktransfer.php
@@ -1,7 +1,8 @@
 <?php
 
-
-class Payment_Offline_Banktransfer extends Payment_Offline {
+use \PHPpayments\Common\Payment_Offline;
+use \PHPpayments\Common\Payment_OfflineInterface;
+class Payment_Offline_Banktransfer extends Payment_Offline implements Payment_OfflineInterface {
 
 }
 
